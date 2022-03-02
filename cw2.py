5b14ca0539b2687ba202e2da0f763929e82e23e4
Patryk <patryk.bachanek@wp.pl>
2022-03-02 09:33:48 +0100

    cw 2

---
+++ cw2.py
@@ -0,0 +1,39 @@
+# a = 6
+# b = 5
+
+# if a > b:
+    # print("Liczba a jest wieksza od liczby b")
+
+# elif a < b:
+    # print("Liczba a jest mniejsza od b")
+
+# else:
+   # print("Liczby są równe")
+
+# print(a)
+# print(type(a))
+# a = int(a)
+# print(type(a))
+
+# a = input("Wczytaj liczbe a: ")
+# b = input("Wczytaj liczbe b: ")
+# c = input("Wczytaj liczbe c: ")
+# d = input("Wczytaj liczbe d: ")
+
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# d = int(d)
+
+#if (a > b) & (c > d):
+#    print("Liczba a jest wieksza od liczby b i liczba c jest wieksza od liczby d")
+#else:
+#    print("Liczba a jest mniejsza od liczby b lub liczba c jest mniejsza od liczby d")
+# for a in range(0, 7, 1):
+#     print(a)
+
+lista = ['a', 3, 4, 5.6]
+for a in lista:
+    print(a)
+else:
+    print('wyswietlone zostaly elementy z listy')
