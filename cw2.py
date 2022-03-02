21f5aedfc8a041fe3ca8d8b5f1abd4028b2127a3
Patryk <patryk.bachanek@wp.pl>
2022-03-02 10:44:01 +0100

    lab2

--- cw2.py
+++ cw2.py
@@ -32,8 +32,54 @@
 # for a in range(0, 7, 1):
 #     print(a)
 
-lista = ['a', 3, 4, 5.6]
-for a in lista:
-    print(a)
-else:
-    print('wyswietlone zostaly elementy z listy')
+# lista = ['a', 3, 4, 5.6]
+# for a in lista:
+#     print(a)
+# else:
+#     print('wyswietlone zostaly elementy z listy')
+
+# licznik = 0
+# while licznik != 11;
+#     print(licznik)
+#     licznik += 1
+
+# lista = [4, 6, 2, 5, 3, 9, 8, 7]
+# a = input('Podaj liczbe: ')
+# licznik = 0
+# while licznik != len(lista):
+#     if int(a) - lista[licznik] == 0:
+#         print('liczba ' + str(a) +  ' - ' + str(lista[licznik]) + ' = 0')
+#         break
+#     else:
+#         licznik += 1
+# else:
+#     print('Zadna z  wartosci nie dala odpowiedniego wyniku')
+
+# lista1 = [4, 6, 3, 2, 7, 9, 5, 1]
+# lista2 = [4, 7, 5, 3]
+# suma = []
+# for a in lista1:
+#     for b in lista2:
+#         wynik = a + b
+#         suma.append(wynik)
+#     print(suma)
+
+# a = input("Liczba a: ")
+# b = input("Liczba b: ")
+# try:
+#     a = int(a)
+#     b = int(b)
+#     wynik = a / b
+#     print(wynik)
+# except ZeroDivisionError:
+#     print('nie dzieli sie przez 0')
+# except ValueError:
+#     print('Nie wczytano liczby calkowitej')
+
+lista = [3, 6, 7, 8, 9, 10, 1]
+slownik ={
+    'pies':'7',
+    'lata':'2002'
+}
+print(slownik)
+
